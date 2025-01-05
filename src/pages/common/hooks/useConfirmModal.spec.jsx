0acef48eff462b8761294be124d6c4869ddf15b2
heo-it <yejin097@naver.com>
2025-01-05 14:34:19 +0900

    test: 훅 테스트 작성

--- src/pages/common/hooks/useConfirmModal.spec.jsx
+++ src/pages/common/hooks/useConfirmModal.spec.jsx
@@ -2,8 +2,23 @@ import { renderHook, act } from '@testing-library/react';
 
 import useConfirmModal from './useConfirmModal';
 
-it('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {});
+it('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {
+  const { result } = renderHook(() => useConfirmModal());
 
-it('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {});
+  expect(result.current.isModalOpened).toBe(false);
+});
 
-it('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {});
+it('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {
+  const { result } = renderHook(() => useConfirmModal(true));
+
+  expect(result.current.isModalOpened).toBe(true);
+});
+
+it('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {
+  const { result } = renderHook(useConfirmModal);
+
+  // NOTE: 컴포넌트 내 state를 변경 -> 리렌더링한 뒤의 상태를 테스트해야한다면 act함수로 감싸주어야 한다.
+  act(() => result.current.toggleIsModalOpened());
+
+  expect(result.current.isModalOpened).toBe(true);
+});
